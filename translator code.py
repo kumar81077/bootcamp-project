d62d46822533928132ed5b54da486282bf7cebc9
kumar81077 <86903624+kumar81077@users.noreply.github.com>
2021-07-04 06:30:09 +0100

    delete

--- translator code.py
+++
@@ -1,7 +0,0 @@
-from googletrans import Translator
-# taking some text in spanish language
-text = "me encanta python.hub"
-translator = Translator()
-dt = translator.detect(text)
-translated = translator.translate(text)
-print(translated.text)

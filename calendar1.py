3be553ca579eb82c27842e3c8569465903533d7d
kumar81077 <86903624+kumar81077@users.noreply.github.com>
2021-07-04 06:59:25 +0100

    uploaded the project file

---
+++ calendar1.py
@@ -0,0 +1,8 @@
+import calendar
+yy = 1997     # year
+mm = 1       # month
+# To take month and year input from the user
+# yy = int(input("Enter Year: "))
+# mm = int(input("Enter Month: "))
+# display the calendar
+print(calendar.month(yy, mm))
